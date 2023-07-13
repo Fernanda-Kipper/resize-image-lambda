a7b7a901c877aba28d3eaebf7000ed8b5c30f012
Fernanda Kipper <nanda.kipper@gmail.com>
2023-07-13 10:38:17 -0300

    feat: add logic to resize images

--- index.mjs
+++ index.mjs
@@ -1,8 +1,71 @@
+import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { Readable } from 'stream';
+import sharp from 'sharp';
+
+const client = new S3Client({ region: 'us-east-1' })
+
 export const handler = async(event) => {
-    // TODO implement
-    const response = {
+
+    if(event?.Records && event.Records.length > 0) {
+        for(record of event.Records){
+            const bucketName = record.s3.bucket.name;
+            const objectKey = record.object.key;
+        
+            const command = new GetObjectCommand({
+                Bucket: bucketName,
+                Key: objectKey
+            });
+
+            try {
+                const response = await client.send(command);
+                var stream = response.Body;
+                if (stream instanceof Readable) {
+                    var content_buffer = Buffer.concat(await stream.toArray());
+                    const width  = 200;
+
+                    try {    
+                    var output_buffer = await sharp(content_buffer).resize(width).toBuffer();
+
+                    await putObject(bucketName + '-resized', objectKey, output_buffer);
+
+                    } catch (error) {
+                        console.log(error);
+                        return;
+                    }
+                } else {
+                    throw new Error('Unknown object stream type');
+                }
+
+            } catch (error) {
+                return {
+                    statusCode: 500,
+                    body: 'Error getting object from bucket',
+                }
+            }
+        }
+    }
+ 
+    return {
         statusCode: 200,
-        body: JSON.stringify('Teste!'),
+        body: 'Image resized successfully',
     };
-    return response;
 };
+
+async function putObject(dstBucket, dstKey, content) {
+    try {
+        const destparams = {
+          Bucket: dstBucket,
+          Key: dstKey,
+          Body: content,
+          ContentType: "image"
+        };
+      
+        const putResult = await s3.send(new PutObjectCommand(destparams));
+
+        return putResult;
+      
+        } catch (error) {
+          console.log(error);
+          return;
+        }
+}
