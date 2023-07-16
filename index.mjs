fed3069f0a1a2c8b83c9c73ae00d18c0806c004c
Fernanda Kipper <nanda.kipper@gmail.com>
2023-07-16 09:47:22 -0300

    chore: add debug log

--- index.mjs
+++ index.mjs
@@ -7,6 +7,7 @@ const client = new S3Client({ region: 'us-east-1' })
 export const handler = async(event) => {
 
     if(event?.Records && event.Records.length > 0) {
+        console.log(event.Records)
         for(record of event.Records){
             const bucketName = record.s3.bucket.name;
             const objectKey = record.object.key;
