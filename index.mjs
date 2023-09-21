e332fce3122b3e86ae4ec6e821b252f1c40bcda8
Fernanda Kipper <nanda.kipper@gmail.com>
2023-09-20 21:00:22 -0300

    refactor: extract functions

--- index.mjs
+++ index.mjs
@@ -5,44 +5,13 @@ import sharp from 'sharp';
 const client = new S3Client({ region: 'us-east-1' })
 
 export const handler = async(event) => {
-    if(event?.Records && event.Records.length > 0) {
-        for(const record of event.Records){
-            const bucketName = record.s3.bucket.name;
-            const objectKey = record.s3.object.key;
-            const objectType = record.s3.object.type;
-        
-            const getCommand = new GetObjectCommand({
-                Bucket: bucketName,
-                Key: objectKey
-            });
-
-            try {
-                const response = await client.send(getCommand);
-                var stream = response.Body;
-                if (stream instanceof Readable) {
-                    var content_buffer = Buffer.concat(await stream.toArray());
-                    const width  = 200;
-
-                    try {    
-                    var output_buffer = await sharp(content_buffer).resize(width).toBuffer();
-
-                    await putObject('thumbnails-output-file', objectKey, output_buffer, );
-
-                    } catch (error) {
-                        console.log(error);
-                        return;
-                    }
-                } else {
-                    throw new Error('Unknown object stream type');
-                }
-
-            } catch (error) {
-                return {
-                    statusCode: 500,
-                    body: 'Error getting object from bucket',
-                }
-            }
-        }
+    try {
+        resizeEachFile(event)
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: error.message,
+        };
     }
  
     return {
@@ -50,6 +19,53 @@ export const handler = async(event) => {
         body: 'Image resized successfully',
     };
 };
+
+async function resizeEachFile(event){
+    if(event?.Records && event.Records.length > 0) {
+        for(const record of event.Records){
+            const bucketName = record.s3.bucket.name;
+            const objectKey = record.s3.object.key;
+
+            try {
+                let originObject = getObject(bucketName, objectKey);
+
+                var stream = originObject.Body;
+                if (stream instanceof Readable) {
+                    var content_buffer = Buffer.concat(await stream.toArray());
+                    const width = 200;
+                
+                    try {    
+                    var output_buffer = await sharp(content_buffer).resize(width).toBuffer();
+                
+                    await putObject('thumbnails-output-file', objectKey, output_buffer, );
+                
+                    } catch (error) {
+                        throw new Error('Error while uploading new object');
+                    }
+                } else {
+                    throw new Error('Unknown object stream type');
+                }
+
+            } catch(e) {
+                throw new Error('Error getting object from bucket');
+            }
+        }
+    }
+}
+
+async function getObject(bucketName, objectKey){
+    const getCommand = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: objectKey
+    });
+
+    try {
+        const response = await client.send(getCommand);
+        return response;
+    } catch (error) {
+        throw new Error('Error getting object from bucket');
+    }
+}
 
 async function putObject(dstBucket, dstKey, content) {
     try {
